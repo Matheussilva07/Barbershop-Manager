73c745837f08fd9e837da273555521eff665d16e
Matheus <theusantana123@hotmail.com>
2025-04-12 15:47:47 -0300

    configuring the Program class

--- src/BarbershopManager.API/Program.cs
+++ src/BarbershopManager.API/Program.cs
@@ -14,6 +14,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://*:{port}");
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
